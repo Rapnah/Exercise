539d31ef46c5f77e9481cbcef149130359d78d08
Rapnah <tranducuy15@gmail.com>
2023-09-29 16:48:53 +0700

    Execrise 2

---
+++ Prime Numbers/Check_main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int i;
+	int count;
+	cout << "Nhap So Can Kiem Tra: ";
+	cin >> i;
+	for (int n = 1; n < i; n++)
+	{
+		if (i % n == 0)
+		{
+			count++;
+		}
+	}
+	if(count == 2)
+	{
+		cout << "Day la so nguyen to !!";
+	}
+	else
+	{
+		cout << "Day khong phai la so nguyen to !!";
+	}
+}
