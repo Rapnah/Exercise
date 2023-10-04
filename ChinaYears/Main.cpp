28125f6790e20dc03d8190659beb202f962065ea
Rapnah <tranducuy15@gmail.com>
2023-10-04 15:32:44 +0700

    Exercise 4

---
+++ ChinaYears/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+char canchar[][10]={"Canh","Tan","Nham","Quy","Giap","At","Binh","Dinh","Mau","Ky"};
+char chichar[][12]={"Than","Dau","Tuat","Hoi","Ty","Suu","Dan","Meo","Thin","Ty","Ngo","Mui"};
+
+int main()
+{
+    int year,can,chi;
+    cout << "Nhap nam can kiem tra: ";
+    cin >> year;
+    can = year % 10;
+    chi = year % 12;
+    cout<< canchar[can] << " "<< chichar[chi] << endl;
+
+}
